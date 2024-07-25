bef5b0b86471402c5dadb7c53cf83fe17001114f
wasim-scene <wasim-scene@outlook.com>
2024-07-25 18:00:23 +1000

    Update ping_glitch.py

--- ping_glitch.py
+++ ping_glitch.py
@@ -1,12 +1,29 @@
 import requests
 import time
+import threading
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
 
 url = "https://storm-meowing-card.glitch.me/"
+@app.route('/')
+def index():
+    return "Your Flask server is running!"
 
-while True:
-    try:
-        response = requests.get(url)
-        print(f"Pinged {url} with status code {response.status_code}")
-    except Exception as e:
-        print(f"Failed to ping {url}: {e}")
-    time.sleep(300)  # Sleep for 5 minutes
+def run_script():
+    while True:
+        try:
+            response = requests.get(url)
+            print(f"Pinged {url} with status code {response.status_code}")
+        except Exception as e:
+            print(f"Failed to ping {url}: {e}")
+        time.sleep(300)
+            pass
+
+if __name__ == "__main__":
+    # Run the long-running script in a separate thread
+    threading.Thread(target=run_script).start()
+    app.run(host='0.0.0.0', port=8080)
+
